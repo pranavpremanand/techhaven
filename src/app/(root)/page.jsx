de8cf989523058619ce9f03b62d2635450b09c88
NandhuNarayanan <nandhunaznarayan@gmail.com>
2025-03-29 11:51:05 +0530

    removed items

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -118,7 +118,7 @@ export default function Home() {
       </section>
       {/* <FeaturedProductsSection /> */}
       {/* <EditorsBestChoice /> */}
-      <BrandLogos />
+      {/* <BrandLogos /> */}
     </div>
   );
 }
